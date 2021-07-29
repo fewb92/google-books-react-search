02cccc4d8ca8385c67db9703d4631a9a4f829881
fewb92 <sotojosh96@gmail.com>
2021-07-29 17:27:00 -0500

    installed react router dependency, began setting it up, created mybooks component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import Nav from './components/Nav'
 import BookSearch from './components/BookSearch'
+import MyBooks from './components/MyBooks'
+import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <BookSearch />
-    </div>
+    <Router>
+      <Route exact path="/"
+    </Router>
   );
 }
 
---
+++ client/src/components/MyBooks.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MyBooks() {
+    return (
+        <div>
+            <h1>My Books</h1>
+        </div>
+    )
+}
