4129203cb45acf0d6c629fda7b3670d74c36cda4
fewb92 <sotojosh96@gmail.com>
2021-07-29 18:22:22 -0500

    fixed naming issue that prevented npm start from running backend

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const search = require('./services/googleBooksRoutes');
+const search = require('./services/googleBooksAPI');
 
 router.use('/api', apiRoutes);
 router.use('/search', search);
